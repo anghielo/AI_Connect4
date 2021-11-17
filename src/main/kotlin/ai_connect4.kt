9cea84b21860387e1d029f6840e4415567e9a389
Pahok9 <90938794+Pahok9@users.noreply.github.com>
2021-11-16 18:08:08 -0800

    Revert "Exit option but for real this time"
    This reverts commit ef9fd6054cd4000ae4f36c6c3594b6500433a2bb.

--- src/main/kotlin/ai_connect4.kt
+++ src/main/kotlin/ai_connect4.kt
@@ -82,14 +82,14 @@ fun main(args: Array<String>) {
 
 private fun gameLoop(b: Board) {
     var inGame: Boolean = true
-    loop@ while (inGame) {
+    while (inGame) {
         while (true) {
             print("Enter your move. (Ex: d5)\n==> ")
             val move: String = keyboard_input.nextLine()
             if (move == "exit")
             {
                 inGame = false
-                break@loop
+                break
             }
             var x = 1
             var y = 1
